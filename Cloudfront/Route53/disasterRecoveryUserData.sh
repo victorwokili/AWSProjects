098702b35c43fd417890bee97f45087ccaacf0b1
Victor Wokili <vwokili@gmail.com>
2023-05-19 10:30:29 -0700

    Update disasterRecoveryUserData.sh

--- Cloudfront/Route53/disasterRecoveryUserData.sh
+++ Cloudfront/Route53/disasterRecoveryUserData.sh
@@ -2,6 +2,6 @@
 yum update -y 
 yum install httpd -y 
 cd /var/www/html 
-echo "JJ Tech Inc Disaster Recovery strategy include using Route53 Failover Based Routing" > index.html 
+echo "Disaster Recovery strategy include using Route53 Failover Based Routing" > index.html 
 service httpd start 
 chkconfig httpd on 
