55a8708e82a0f6d2337409c180f493d3a8da69f7
Victor Wokili <vwokili@gmail.com>
2023-05-26 12:35:58 -0700

    Delete Cloudfront directory

--- Cloudfront/Route53/disasterRecoveryUserData.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-yum update -y 
-yum install httpd -y 
-cd /var/www/html 
-echo "Disaster Recovery strategy include using Route53 Failover Based Routing" > index.html 
-service httpd start 
-chkconfig httpd on 
--- Cloudfront/Route53/mainuserdata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-yum update -y 
-yum install httpd -y 
-cd /var/www/html 
-echo "Today, we will be exploring all the routing policies including failover based routing, weighted based routing, latency based routing, geolocation and simple based routing policies" > index.html 
-service httpd start 
-chkconfig httpd on 
